fd78a1af4fb65e99a8c94fe3417497445a685375
SirWoodhouse <jonger_dartelen0a@icloud.com>
2024-12-16 22:10:02 +0100

    On branch main

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
+## Imports
+# regex for advanced string matching
+
+import re
+
+## Global variables
+# Unique values in the start of the string
 uncounters = {}
+# Unique values, end of the string
 nocounters = {}
+# Total items
 totalcount = 0
 
-file = open('applicants.csv')
-next(file) # exclude column header from counts
-for line in file:
-    totalcount = totalcount + 1
-    un, number = line[0:2], line[2:3]
-    if un not in uncounters:
-        uncounters[un] = 1
-    else:
-        uncounters[un] += 1
-    if number not in nocounters:
-        nocounters[number] = 1
-    else:
-        nocounters[number] += 1
+## Main program
+def main(file):
+    global totalcount
+    next(file)  # Skip first entry (column header)
+    for line in file:
+        # Get all the letters at the start of the string
+        un = re.search(r"^\D+", line).group()
+        # Get the first number in the string
+        no = re.search(r"\d{1}", line).group()
+        # DEBUG: Print variables to verify
+        # print(f"un:{un}, no:{no}")
+        # Process un
+        addtolist(un, uncounters)
+        # Process no
+        addtolist(no, nocounters)
+        # Raise counter
+        totalcount += 1
+    #Print results
+    print("===")
+    print(f'The are a total of {totalcount} applicants.')
+    print("===")
+    printlist("Applicants per department", uncounters)
+    printlist("Applicants per seniority level", nocounters)
 
-print('The are a total of ', totalcount, ' applicants', '.', sep = '')
-print()
-print('Applicants per department:')
-for key,value in uncounters.items():
-     print(key, value)
-print()
-print('Applicants per seniority level:')
-for key, value in nocounters.items():
-    print(key, value)
+## Helper functions
+def addtolist(value, list):
+    if value is not None:
+        if value not in list:
+            list[value] = 1
+        else:
+            list[value] += 1
+
+def printlist(title, list):
+    print(f"{title}:")
+    for key, value in list.items():
+        print(f"{key}:\t{value}")
+    print("===")
+
+## Intializer
+if __name__ == "__main__":
+    file = open('applicants.csv')
+    main(file)
